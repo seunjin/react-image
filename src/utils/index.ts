6a62c9e14c4888d7b9c8cf8e31c9c1ffa9ea8ff0
seunjin <jinseun@gmail.com>
2025-07-17 19:38:02 +0900

    feat: filter 컴포넌트 코드 작성

---
+++ src/utils/index.ts
@@ -0,0 +1,14 @@
+import {ImageFilter} from '$type'
+
+type CSSFilterValue = NonNullable<CSSStyleDeclaration['filter']>
+
+export function getFilter({grayscale = 0, sepia = 0, brightness = 100, contrast = 100, blur = 0}: ImageFilter = {}): {
+    filter: CSSFilterValue
+} {
+    const filterValue: CSSFilterValue =
+        `grayscale(${grayscale}%) sepia(${sepia}%) brightness(${brightness}%) contrast(${contrast}%) blur(${blur}px)`.trim()
+
+    return {
+        filter: filterValue,
+    }
+}
